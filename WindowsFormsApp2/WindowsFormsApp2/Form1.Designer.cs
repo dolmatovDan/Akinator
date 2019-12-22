94a2d7f59e782b4b4ce8cebe4df91a0e9d075c8a
Unknown <dolmatovdan2006@gmail.com>
2019-12-22 11:19:50 +0300

    All proyect

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Location = new System.Drawing.Point(13, 23);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(534, 186);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(198, 364);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(377, 87);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Начать игру";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Button1_KeyDown1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(198, 316);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(377, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Button1_KeyDown1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(566, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(235, 247);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = false;
+            pictureBox1.Visible = false;
+            textBox1.Visible = false;
+            
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+          
+
+        }
+        int step = 0;
+        void game()
+        {
+            if (step == 0)
+            {
+                pictureBox1.Visible = false;
+                textBox1.Visible = true;
+                
+                richTextBox1.Text = "Персонаж человек?\n Нужно ответить да/нет";
+                button1.Text = "Следующий вопрос";
+                step++;
+                textBox1.Text = "";
+            }
+            else if (step == 1)
+            {
+
+                if (textBox1.Text.ToLower() == "да")
+                {
+                    richTextBox1.Text = "Это мужчина?\n Нужно ответить да/нет";
+                    step++;
+                }
+                else if (textBox1.Text.ToLower() == "нет")
+                {
+                    richTextBox1.Text = "Он демон?";
+                    step = 6;
+                }
+            }
+            else if (step == 6)
+            {
+
+                if (textBox1.Text.ToLower() == "да")
+                {
+                    richTextBox1.Text = "Это Кроули";
+                    pictureBox1.Visible = true;
+                    pictureBox1.Image = Properties.Resources.krouli;
+                    step = 12;
+                    startGame();
+                }
+                else if (textBox1.Text.ToLower() == "нет")
+                {
+                    richTextBox1.Text = "Это ангел?\n Нужно ответить да/нет";
+                    step++;
+                }
+            }
+            else if (step == 7)
+            {
+                if (textBox1.Text.ToLower() == "да")
+                {
+                    richTextBox1.Text = "Это Кастиэль?\n Нужно ответить да/нет";
+                    step++;
+                }
+                else if (textBox1.Text.ToLower() == "нет")
+                {
+                    richTextBox1.Text = "Это король вампиров";
+                    pictureBox1.Visible = true;
+                    pictureBox1.Image = Properties.Resources.alfa;
+                    step = 12;
+                    startGame();
+                }
+            }
+            else if (step == 8)
+            {
+                if (textBox1.Text.ToLower() == "да")
+                {
+                    richTextBox1.Text = "Это Кастиэль";
+                    pictureBox1.Visible = true;
+                    pictureBox1.Image = Properties.Resources.kas;
+                    step = 12;
+                    startGame();
+                }
+                else if (textBox1.Text.ToLower() == "нет")
+                {
+                    richTextBox1.Text = "Он хороший?";
+                    step++;
+                }
+            }
+            else if (step == 9)
+            {
+                if (textBox1.Text.ToLower() == "да")
+                {
+                    richTextBox1.Text = "Это Чак";
+                    pictureBox1.Visible = true;
+                    pictureBox1.Image = Properties.Resources.chak;
+                    step = 12;
+                    startGame();
+                }
+                else if (textBox1.Text.ToLower() == "нет")
+                {
+                    richTextBox1.Text = "Это Люцифер";
+                    pictureBox1.Visible = true;
+                    pictureBox1.Image = Properties.Resources.lucifer;
+                    step = 12;
+                    startGame();
+                }
+            }
+            else if (step == 2)
+            {
+
+                if (textBox1.Text.ToLower() == "да")
+                {
+                    richTextBox1.Text = "Это Винчестер?\n Нужно ответить да/нет";
+                    step++;
+
+                }
+                else if (textBox1.Text.ToLower() == "нет")
+                {
+                    richTextBox1.Text = "У неё есть дети?";
+                    step = 11;
+                }
+            }
+            else if (step == 3)
+            {
+
+                if (textBox1.Text.ToLower() == "да")
+                {
+                    richTextBox1.Text = "Это Сэм?\n Нужно ответить да/нет";
+                    step++;
+
+                }
+                else if (textBox1.Text.ToLower() == "нет")
+                {
+                    pictureBox1.Visible = true;
+                    pictureBox1.Image = Properties.Resources.boby;
+                    step = 12;
+                    startGame();
+                }
+            }
+            else if (step == 4)
+            {
+
+                if (textBox1.Text.ToLower() == "да")
+                {
+                    richTextBox1.Text = "Это Сэм?";
+                    pictureBox1.Visible = true;
+                    pictureBox1.Image = Properties.Resources.sam;
+                    step = 12;
+                    startGame();
+                }
+                else if (textBox1.Text.ToLower() == "нет")
+                {
+                    richTextBox1.Text = "Это Дин?\n Нужно ответить да/нет";
+                    step++;
+                }
+            }
+            else if (step == 5)
+            {
+
+                if (textBox1.Text.ToLower() == "да")
+                {
+                    richTextBox1.Text = "Это Дин";
+                    pictureBox1.Visible = true;
+                    pictureBox1.Image = Properties.Resources.din;
+
+                }
+                else if (textBox1.Text.ToLower() == "нет")
+                {
+                    richTextBox1.Text = "Это Джон";
+                    pictureBox1.Visible = true;
+                    pictureBox1.Image = Properties.Resources.jon;
+                }
+                step = 12;
+                startGame();
+            }
+            else if (step == 11)
+            {
+
+                if (textBox1.Text.ToLower() == "да")
+                {
+                    richTextBox1.Text = "Это Джо";
+                    pictureBox1.Visible = true;
+                    pictureBox1.Image = Properties.Resources.jo;
+
+                }
+                else if (textBox1.Text.ToLower() == "нет")
+                {
+                    richTextBox1.Text = "Это Эллен";
+                    pictureBox1.Visible = true;
+                    pictureBox1.Image = Properties.Resources.ellen;
+
+                }
+                step = 12;
+                startGame();
+
+            }
+            else if (step == 12)
+            {
+                step = 0;
+                pictureBox1.Visible = false;
+                button1.PerformClick();
+                
+            }
+
+
+        }
+
+        private void startGame()
+        {
+                this.KeyPreview = true;
+                pictureBox1.Visible = false;
+       
+
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+                private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            game();
+        }
+        private void Button1_KeyDown1(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                game();
+            }
+            
+        }
+
+    }
+}
